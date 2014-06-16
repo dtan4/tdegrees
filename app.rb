1163678c6899180525b426aa8c6a11b42ccbf6eb
dtan4 <dtanshi45@gmail.com>
2014-06-16 13:56:03 +0900

    s/TDEGREE/TDEGREES/g

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require "twitter"
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV["TDEGREE_CONSUMER_KEY"]
-  config.consumer_secret = ENV["TDEGREE_CONSUMER_SECRET"]
-  config.access_token = ENV["TDEGREE_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TDEGREE_ACCESS_SECRET"]
+  config.consumer_key = ENV["TDEGREES_CONSUMER_KEY"]
+  config.consumer_secret = ENV["TDEGREES_CONSUMER_SECRET"]
+  config.access_token = ENV["TDEGREES_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TDEGREES_ACCESS_SECRET"]
 end
